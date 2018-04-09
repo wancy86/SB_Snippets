5e3475119c2fe9c58a8e64eee83ea4bd241e4c54
wancy86 <wancy86@sina.com>
2018-04-09 09:12:05 +0800

    comment: new snippet

--- CopyToSB.bat
+++ CopyToSB.bat
@@ -1,3 +1,4 @@
 copy /y "D:\GitHub\sublime text snippets\*.sublime-snippet" "C:\Program Files\Sublime Text 3\Data\Packages\User\"
+::explorer C:\Program Files\Sublime Text 3\Data\Packages\User\
 
 pause
